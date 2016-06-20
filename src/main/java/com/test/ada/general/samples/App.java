efc99a5858e0b7ac0135c670a1c26e08dc0aec30
Sid <Sid@Sid-PC.T-mobile.com>
2016-06-20 00:46:24 -0400

    initial push

---
+++ src/main/java/com/test/ada/general/samples/App.java
@@ -0,0 +1,13 @@
+package com.test.ada.general.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
